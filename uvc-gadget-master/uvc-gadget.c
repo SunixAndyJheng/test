8e3de7f647892e98dbf86b3c396dcfb1f3fbe588
AndyJheng <andy_jheng@sunix.com>
2026-03-18 17:48:41 +0800

    second change

--- uvc-gadget-master/uvc-gadget.c
+++ uvc-gadget-master/uvc-gadget.c
@@ -2058,6 +2058,7 @@ static void usage(const char *argv0)
 int main(int argc, char *argv[])
 {
 	printf("test\n");
+	int a = a + 1;
     struct uvc_device *udev;
     struct v4l2_device *vdev;
     struct timeval tv;
